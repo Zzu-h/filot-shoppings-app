1e55b4c3ce4c1332479cc8323ebdf94501731c68
Zzu-h <juho@juhoui-MacBookPro.local>
2022-02-25 17:28:17 +0900

    last update'

--- app/src/main/java/com/zzuh/filot_shoppings/data/repository/ProductListRepository.kt
+++ app/src/main/java/com/zzuh/filot_shoppings/data/repository/ProductListRepository.kt
@@ -1,13 +1,14 @@
 package com.zzuh.filot_shoppings.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.zzuh.filot_shoppings.data.datasource.ProductListNetworkDataSource
 import com.zzuh.filot_shoppings.data.vo.ProductList
 
 class ProductListRepository {
     var dataSource: ProductListNetworkDataSource = ProductListNetworkDataSource()
-    init {
-    }
+    var productList = MutableLiveData<ProductList>()
+
     fun fetchProductList(name: String): LiveData<ProductList>{
         dataSource.fetchProductList(name)
         return dataSource.downloadProductListResponse
--- app/src/main/java/com/zzuh/filot_shoppings/ui/main/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/zzuh/filot_shoppings/ui/main/viewmodel/CategoryViewModel.kt
@@ -1,19 +1,25 @@
 package com.zzuh.filot_shoppings.ui.main.viewmodel
 
+import android.app.Activity
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.zzuh.filot_shoppings.data.repository.ProductListRepository
 import com.zzuh.filot_shoppings.data.vo.ProductList
 
-class CategoryViewModel:ViewModel() {
+class CategoryViewModel(activity: Activity):ViewModel() {
     var categoryName = MutableLiveData<String>()
     var productListRepository = ProductListRepository()
     var productList = MutableLiveData<ProductList>()
+    init {
+        productListRepository.productList.observe(context, Observer {
 
+        })
+    }
     fun getProductList(name: String): LiveData<ProductList>{
         var list = productListRepository.fetchProductList(name)
-        println("print: ${list.value?.products.toString()}")
         productList.postValue(list.value)
 
         return list
