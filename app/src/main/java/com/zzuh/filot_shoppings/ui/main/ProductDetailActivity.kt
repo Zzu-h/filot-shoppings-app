6f2c10ef4edf89432890faef3d9280b17d98b6a4
Zzu-h <juho7785@naver.com>
2022-03-01 22:26:09 +0900

    update: detail activity(image rv error)

--- app/src/main/java/com/zzuh/filot_shoppings/ui/main/ProductDetailActivity.kt
+++ app/src/main/java/com/zzuh/filot_shoppings/ui/main/ProductDetailActivity.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,13 +17,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
+import com.zzuh.filot_shoppings.R
 import com.zzuh.filot_shoppings.data.vo.SelectedProductItem
 import com.zzuh.filot_shoppings.databinding.ActivityProductDetailBinding
 import com.zzuh.filot_shoppings.databinding.ImageListItemBinding
 import com.zzuh.filot_shoppings.ui.main.viewmodel.DetailsViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class ProductDetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityProductDetailBinding
@@ -28,6 +34,9 @@ class ProductDetailActivity : AppCompatActivity() {
 
     lateinit var selectedListAdapter: SelectedListAdapter
     lateinit var imageListAdapter: ImageListAdapter
+
+    lateinit var sizeList: List<String>
+    lateinit var colorList: List<String>
 
     val productId: Int by lazy {
         intent.getIntExtra("id", 0)
@@ -51,8 +60,30 @@ class ProductDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.backBtn.setOnClickListener { finish() }
 
+        viewModelSetting()
+    }
+    private fun spinnerSetting(){
+        val sizeAdapter = ArrayAdapter(this, R.layout.spinner_dropdown_item, sizeList)
+        val colorAdapter = ArrayAdapter(this, R.layout.spinner_dropdown_item, colorList)
+        binding.productSizeSpinner.adapter = sizeAdapter
+        binding.productColorSpinner.adapter = colorAdapter
+        binding.productSizeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Log.d("Tester", "selected $position")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Log.d("Tester", "nothing")
+            }
+        }
+    }
+    private fun viewModelSetting(){
         detailsViewModel.product.observe(this, Observer {
             CoroutineScope(Dispatchers.Main).launch {
+                for(item in it.images)
+                    Glide.with(this@ProductDetailActivity)
+                        .load(item)
+
                 binding.itemTitle.text = it.name
                 binding.productPriceItem.text = "KRW ${it.price}"
                 it.description?.let {
@@ -65,7 +96,13 @@ class ProductDetailActivity : AppCompatActivity() {
                         .load(thumbnail)
                         .into(binding.productImg)
                 }
+                colorList = it.colors
+                sizeList = it.size.split(",")
+                spinnerSetting()
+                delay(500)
                 imageListAdapter.updateData(it.images)
+                binding.loadingBar.visibility = View.GONE
+                binding.detailContentLayout.visibility = View.VISIBLE
             }
         })
         detailsViewModel.selectedList.observe(this, Observer { selectedListAdapter.updateData(it.list as List<SelectedProductItem>) })
@@ -79,9 +116,11 @@ class ImageListAdapter(private var itemList: List<String>): RecyclerView.Adapter
     override fun onBindViewHolder(holder: ImageListViewHolder, position: Int) {
         val binding = holder.binding
         val item = this.itemList[position]
-        Log.d("Tester", item+"${itemList.size}")
+        Log.d("Tester", item)
         Glide.with(context)
             .load(item)
+            .fitCenter()
+            .override(Target.SIZE_ORIGINAL)
             .into(binding.descriptionIv)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageListViewHolder {
